5c3e2bae70d8f282061de747ba60a3b9c2ed69c8
Jason Zhao <jasonjinzhao99@gmail.com>
2019-05-14 22:09:17 -0700

    small tests on test_abnormal.py

--- src/test_abnormal.py
+++ src/test_abnormal.py
@@ -2,4 +2,31 @@ from tensorflow.keras.models import load_model
 from utils import load_X_and_Y
 
 def test_abnormal(model_path):
+    # load model
     model = load_model(model_path)
+
+    # load data
+    X, Y = load_X_and_Y()
+    _, _, x_test = X
+    _, _, y_test = Y
+
+    # test model on abnormal categories
+
+    print(len(x_test))
+    print(len(y_test))
+    
+    count = 0
+    for x_test_data in x_test:
+        if count > 5: 
+            break
+        print(x_test_data)
+        count += 1
+
+    count = 0
+    for y_test_label in y_test:
+        if count > 5: 
+            break
+        print(y_test_label)
+        count += 1
+
+
