842f67f13caa8766bc22835e65ab652e7d5697f5
Jason Zhao <jasonjinzhao99@gmail.com>
2019-05-14 22:13:37 -0700

    updated test_abnormal.py

--- src/test_abnormal.py
+++ src/test_abnormal.py
@@ -1,32 +1,41 @@
 from tensorflow.keras.models import load_model
 from utils import load_X_and_Y
 
-def test_abnormal(model_path):
-    # load model
-    model = load_model(model_path)
+model_path = "~/openi-chest-xrays/src/cnn_baseline.h5"
 
-    # load data
-    X, Y = load_X_and_Y()
-    _, _, x_test = X
-    _, _, y_test = Y
+class TestAbnormal():
+    def test_top_eight_abnormalities(self, model_path):
+        # load model
+        model = load_model(model_path)
 
-    # test model on abnormal categories
+        # load data
+        X, Y = load_X_and_Y()
+        _, _, x_test = X
+        _, _, y_test = Y
 
-    print(len(x_test))
-    print(len(y_test))
-    
-    count = 0
-    for x_test_data in x_test:
-        if count > 5: 
-            break
-        print(x_test_data)
-        count += 1
+        # test model on abnormal categories
 
-    count = 0
-    for y_test_label in y_test:
-        if count > 5: 
-            break
-        print(y_test_label)
-        count += 1
+        print(len(x_test))
+        print(len(y_test))
+
+        count = 0
+        for x_test_data in x_test:
+            if count > 5: 
+                break
+            print(x_test_data)
+            count += 1
+
+        count = 0
+        for y_test_label in y_test:
+            if count > 5: 
+                break
+            print(y_test_label)
+            count += 1
+
+if __name__ == "__main__":
+    test = TestAbnormal()
+    test.test_top_eight_abnormalities(model_path)
+
+
 
 
